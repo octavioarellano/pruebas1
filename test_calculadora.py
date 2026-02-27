e51878402d3c8d537b69156d68b0beb26dfbf757
Octavio <octavio.arellano@uabc.edu.mx>
2026-02-27 11:59:07 -0800

    Añadiendo archivo

--- test_calculadora.py
+++ test_calculadora.py
@@ -1,11 +1,5 @@
-class Calculadora:
-    def add(self, a, b):
-        return a + b
-    
-    from calculadora import Calculadora
+from calculadora import Calculadora
 
 def test_add():
     calc = Calculadora()
     assert calc.add(2, 3) == 5
-
-# Cambio para probar
